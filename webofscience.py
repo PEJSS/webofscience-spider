dfc7233f1bd43d23ac34b2450bcb52db9f240ad6
PEJSS <41136793+PEJSS@users.noreply.github.com>
2018-07-19 01:19:11 +0800

    before debug

--- webofscience.py
+++ webofscience.py
@@ -7,19 +7,19 @@ def getLink(url,tag,attribute):
     temp = bso.find_all(tag,class_=attribute)
     return temp
 
-def getInfo(url,PassageName=[],Info=[],Used=[],Summary=[],AuthorInfo=[],ReferInfo=[]):
+def getInfo(url,PassageName=[],Info=[],Used=[],AuthorInfo=[],ReferInfo=[]):
     html = urlopen(url)
     bso = BeautifulSoup(html.read(),"lxml")
     passagename = bso.find(PassageName[0],class_=PassageName[1]).value.string
     info = bso.find_all(Info[0],class_=Info[1])
     usedtimes = bso.find(Used[0],class_=Used[1]).string
     authorinfo = bso.find_all(AuthorInfo[0],class_=AuthorInfo[1])
-    SU = bso.find_all(Summary[0],class_=Summary[1])
+    #SU = bso.find_all(Summary[0],class_=Summary[1])
     referinfo = bso.find(ReferInfo[0],attrs={'title':ReferInfo[1]})
     link = 'http://apps.webofknowledge.com'+referinfo['href']
-    for item in SU:
-        if item.string == 'Abstract':
-            summary = item.parent.find('p',class_='FR_field').string
+    #for item in SU:
+        #if item.string == 'Abstract':
+            #summary = item.parent.find('p',class_='FR_field').string
     for item in info:
         if item.string == 'By:':
             authorname = item.next_sibling.string
@@ -46,22 +46,20 @@ def getInfo(url,PassageName=[],Info=[],Used=[],Summary=[],AuthorInfo=[],ReferInf
         bso2 = BeautifulSoup(html2.read(),"lxml")
         refers = bso2.find_all('div',class_='search-results-item')
         for item in refers:
-            print(item)
-            print(item.find('span',class_='reference-title'))
             authors = item.find_all('span',class_='label')
-            authors = [x.parent.contents[2] for x in authors if x.string == '作者：']
-            # for child in authors:
-                # if child.string == '作者: ':
-                    # author = child.parent.contents[2]
-            if len(authors) == 0:
-                raise ValueError("No authors found!")
-            refer += item.find('span',class_='reference-title').value.string + '作者：' + author + ';'
-    return [passagename,authorname,publishdate,usedtimes,keywords,summary,address,email,refer]
+            for child in authors:
+                if child.string == '作者: ':
+                    author = child.parent.contents[2]
+            if item.find('span',class_='reference-title') == None:
+                refer += '标题不可用' + '作者：' + author + ';'
+            else:
+                refer += item.find('span',class_='reference-title').value.string + '作者：' + author + ';'
+    return [passagename,authorname,publishdate,usedtimes,keywords,address,email,refer]
 
 with open('data.csv','w',newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['文章标题','作者名字','出版年','被引频次','关键词','摘要','作者地址','电子邮件','参考文献'])
-for i in range(1,391):
+    writer.writerow(['文章标题','作者名字','出版年','被引频次','关键词','作者地址','电子邮件','参考文献'])
+for i in range(2,391):
     journal=getLink("http://apps.webofknowledge.com/summary.do?product=WOS&parentProduct=WOS&search_mode=GeneralSearch&parentQid=&qid=2&SID=8Ev2lmRWkPOJK5J8HhG&&update_back2search_link_param=yes&page=" + str(i), "a","smallV110 snowplow-full-record")
     for item in journal:
         arti='http://apps.webofknowledge.com'+item['href']
@@ -69,7 +67,6 @@ for i in range(1,391):
                   PassageName=['div','title'],
                   Info=['span','FR_label'],
                   Used=['span','large-number'],
-                  Summary=['div','title3'],
                   AuthorInfo=['td','fr_address_row2'],
                   ReferInfo=['a',"View this record's bibliography"])
         with open('data.csv','a',newline='') as csvfile:
